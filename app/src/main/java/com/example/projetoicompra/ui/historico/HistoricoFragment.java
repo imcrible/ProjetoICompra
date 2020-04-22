771375a3f320e7746ca3ac09db19bb48e2e5387c
Samuel Oliveira <samuel.n.m.oliveira@gmail.com>
2020-04-21 21:54:16 -0300

    att

--- app/src/main/java/com/example/projetoicompra/ui/historico/HistoricoFragment.java
+++ app/src/main/java/com/example/projetoicompra/ui/historico/HistoricoFragment.java
@@ -6,13 +6,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.projetoicompra.BD.ICompraViewModel;
 import com.example.projetoicompra.R;
+import com.example.projetoicompra.activity.MainActivity;
 import com.example.projetoicompra.activity.ViewListaActivity;
+import com.example.projetoicompra.adapter.ListaListAdapter;
+import com.example.projetoicompra.model.Lista_Compra;
+
+import java.util.List;
 
 public class HistoricoFragment extends Fragment {
+
+    private ICompraViewModel iCompraViewModel;
+    private RecyclerView recyclerViewLista;
 
 
     public HistoricoFragment() {
@@ -28,6 +42,25 @@ public class HistoricoFragment extends Fragment {
 
         Intent intencao = new Intent(getContext(), ViewListaActivity.class);
         startActivity(intencao);
+
+
+        /*recyclerViewLista = view.findViewById(R.id.recycler_lista);
+
+        recyclerViewLista.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerViewLista.setHasFixedSize(true);
+
+        final ListaListAdapter adapterlista = new ListaListAdapter();
+        recyclerViewLista.setAdapter(adapterlista);
+
+
+        iCompraViewModel = ViewModelProviders.of(this).get(ICompraViewModel.class);
+        iCompraViewModel.getVm_TodaListaCompra().observe(getViewLifecycleOwner(), new Observer<List<Lista_Compra>>() {
+            @Override
+            public void onChanged(List<Lista_Compra> lista_compras) {
+                adapterlista.setListacompras(lista_compras);
+                Toast.makeText(getContext(), "Era para aparecer a lista", Toast.LENGTH_SHORT).show();
+            }
+        });*/
 
 
         return inflater.inflate(R.layout.fragment_historico, container, false);
